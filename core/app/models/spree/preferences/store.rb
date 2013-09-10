3702277352f18a7c729842a97ef9c323b1df3f35
Washington Luiz <huoxito@gmail.com>
2013-09-10 11:08:22 -0300

    Dont set preference on assets:precompile
    Avoid opening a db connection when running assets:precompile to help
    users to deploy their apps to heroku
    
    Fixes #3703

--- core/app/models/spree/preferences/store.rb
+++ core/app/models/spree/preferences/store.rb
@@ -85,7 +85,7 @@ module Spree::Preferences
     end
 
     def should_persist?
-      @persistence and Spree::Preference.table_exists?
+      @persistence && Spree::Preference.connected? && Spree::Preference.table_exists?
     end
 
   end
